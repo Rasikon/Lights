79e6c530fd366a4749a58162f7ccb086924ac88e
Ivan Redreev <redreev.ivan@gmail.com>
2020-10-16 11:28:52 +0500

    Initial Commit

---
+++ Lights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lights
+//
+//  Created by Rasikon on 16.10.2020.
+//  Copyright © 2020 Rasikon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
